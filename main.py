032ae5e849645ad6920b201dbba551031b7dcd32
ASHEESH DHAMACHARLA <asheeshdhamacharla@ASHEESHs-MacBook-Air.local>
2026-01-15 23:19:33 +0530

    k_controller

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ def main():
         target_model=target_model,
         max_k=8,
         entropy_bins=[1.2, 2.2, 3.0],
-        k_values=[8, 4, 2, 0],
+        k_values=[8, 4, 2, 1],
         acceptance_alpha=0.1,
         acceptance_init=1.0,
     )
