46322986967f818ece1bbbad0dc1f6a2bdf92bdc
ASHEESH DHAMACHARLA <asheeshdhamacharla@ASHEESHs-MacBook-Air.local>
2026-01-15 23:55:33 +0530

    fix

--- optimization/cache_manager.py
+++ optimization/cache_manager.py
@@ -6,8 +6,8 @@ class CacheManager:
     def slice_kv_cache(kv_cache: KVCache, prefix_length) -> KVCache:
         new_cache = []
         for k, v in kv_cache:
-            new_k = k[:, :, :, :prefix_length, :].contiguous()
-            new_v = v[:, :, :, :prefix_length, :].contiguous()
+            new_k = k[:, :, :prefix_length, :].contiguous()
+            new_v = v[:, :, :prefix_length, :].contiguous()
             new_cache.append((new_k, new_v))
         return tuple(new_cache)
     @staticmethod
