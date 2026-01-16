558c8444a16880f80a72a3d47a79bbd367a20e70
ASHEESH DHAMACHARLA <asheeshdhamacharla@ASHEESHs-MacBook-Air.local>
2026-01-16 22:46:45 +0530

    k_controller

--- verification/rejection_sampler.py
+++ verification/rejection_sampler.py
@@ -11,11 +11,3 @@ class RejectionSampler:
         next_token = torch.argmax(logits, dim=-1, keepdim=True)
 
         return next_token
-
-    def _slice_kv_cache(self, kv_cache, accepted_tokens):
-        new_cache = []
-        for k,v in kv_cache:
-            new_k = k[:, :, :accepted_tokens, :].contiguous()
-            new_v = v[:, :, :accepted_tokens, :].contiguous()
-            new_cache.append((new_k, new_v))
-        return tuple(new_cache)
