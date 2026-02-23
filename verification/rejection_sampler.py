ee9c94fad4d65026d43ffdcf3568bd4c6ee11004
ASHEESH DHAMACHARLA <asheeshdhamacharla@ASHEESHs-MacBook-Air.local>
2026-02-24 00:31:47 +0530

    rejection handler

--- verification/rejection_sampler.py
+++ verification/rejection_sampler.py
@@ -1,13 +1,11 @@
 import torch
+
 class RejectionSampler:
-    def __init__(self,target_model,draft_model):
-        self.target_model=target_model
-        self.draft_model=draft_model
-        
-    def handle(self,last_committed_token: torch.Tensor,):
-        
+    def __init__(self, target_model, draft_model):
+        self.target_model = target_model
+        self.draft_model = draft_model
 
+    def handle(self, last_committed_token: torch.Tensor):
         logits = self.target_model.forward_next(last_committed_token)
-        next_token = torch.argmax(logits, dim=-1, keepdim=True)
-
+        next_token = self.target_model.select_tokens(logits)
         return next_token
